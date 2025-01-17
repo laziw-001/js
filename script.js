a89f754c927f0a0257a032dc385e804c40055c59
laziw-001 <abduqodirovalaziza866@gmail.com>
2025-01-17 18:49:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let ism = prompt('Ismingizni kiriting!')
+console.log(ism);
+
+let familiya = prompt('Familiya kiriting!')
+console.log(familiya);
+
+let yosh = prompt('Yoshingizni kiriting!')
+console.log(yosh);
+
+let fan = prompt('Yoqtirgan faningiz?')
+console.log(fan);
+
+let joy = prompt('Qayerda yashaysiz?')
+console.log(joy);
+
